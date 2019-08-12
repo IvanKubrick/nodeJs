9ed0c7156d821e892dbec0804a85be78f091843f
Ivan Grushevich <githubemail911@gmail.com>
2019-08-12 08:55:11 +0300

    Add pug (partialy)

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const rootDir = require('./utils/path');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
