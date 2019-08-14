8c0e4e64de5ea94ebb47c8e1a832d7e03d740acd
Ivan Grushevich <githubemail911@gmail.com>
2019-08-14 09:01:55 +0300

    Update templates using pug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found.html'));
+  res.status(404).render('not-found', { docTitle: 'page not found' });
 });
 
 app.listen(3000);
