b51ef46cf27a3d36ff2311355146c1d14092f04c
Ivan Grushevich <githubemail911@gmail.com>
2019-08-17 13:14:15 +0300

    Update templates

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,10 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { docTitle: 'Add product' });
+  res.render('add-product', {
+    docTitle: 'Add product',
+    path: '/admin/add-product'
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
