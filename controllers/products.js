da74b4d5c54b46e9b8ed3d6516213fe498e418c9
Ivan Grushevich <githubemail911@gmail.com>
2019-08-18 19:53:22 +0300

    Update Product model

--- controllers/products.js
+++ controllers/products.js
@@ -14,6 +14,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', { products, docTitle: 'Shop', path: '/' });
+  Product.fetchAll(products => {
+    res.render('shop', { products, docTitle: 'Shop', path: '/' });
+  });
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../utils/path');
+
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (error, fileContent) => {
+    if (error) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +21,13 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => console.log);
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
