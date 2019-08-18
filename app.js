3b8aaf7455e225adfd5fec09a5d810a5d505502c
Ivan Grushevich <githubemail911@gmail.com>
2019-08-18 12:36:13 +0300

    Change template engine (from pug to ejs)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('not-found', { docTitle: 'page not found' });
+  res.status(404).render('404', { docTitle: 'Page not found', path: '' });
 });
 
 app.listen(3000);
